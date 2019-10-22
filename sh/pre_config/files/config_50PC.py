0b0699880a1c2de49b353b997737b2b099aca77d
maany <imptodefeat@gmail.com>
2019-10-22 18:05:11 +0200

    fix ALLOW_WRITE and add utils to Dockerfile

--- sh/pre_config/files/config_50PC.py
+++ sh/pre_config/files/config_50PC.py
@@ -10,7 +10,7 @@ class BatchConfig(ConfigFile):
         super().add_advanced_parameters()
         execution_id = self.lightweight_component['execution_id']
         dns = get_dns_info(self.augmented_site_level_config, execution_id)
-        allow_write = '.'.join((dns['container_ip'].split('.')[0:-2] + ['*', '*']))
+        allow_write = '.'.join((dns['container_ip'].split('.')[0:-2] + ['*']))
         self.advanced_category.add("Use ROLE: CentralManager\n")
         self.advanced_category.add_key_value("condor_host", dns['container_ip'])
         self.advanced_category.add_key_value("allow_write", allow_write)
