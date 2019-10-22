54e12f0a67e6f7f22b030c3f64a2473159051cf3
maany <imptodefeat@gmail.com>
2019-10-22 15:54:32 +0200

    refactor init script

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cp $SIMPLE_CONFIG_DIR/config/50PC.config $HTCONDOR_CONFIG_DIR/config.d/50PC.config
+echo "----------------------------------"
+echo "Initializing HTCondor Execute Node"
+echo "----------------------------------"
+echo "Copying config files from $SMIPLE_CONFIG_DIR/config to $HTCONDOR_CONFIIG_DIR/config.d"
+cp $SIMPLE_CONFIG_DIR/config/50PC.conf $HTCONDOR_CONFIG_DIR/config.d/50PC.conf
+echo "----------------------------------"
+echo "Starting daemons"
+echo "----------------------------------"
+echo "Starting HTCondor"
 systemctl start condor
+echo "Starting crond"
+systemctl start crond
+
+echo "Initialization Complete!"
