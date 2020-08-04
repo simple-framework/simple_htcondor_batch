dcd4737abdf16e476803a76ab923d1d790f678fa
maany <imptodefeat@gmail.com>
2020-08-04 15:28:50 +0200

    generate acct_groups and quotas

--- sh/pre_config/files/accounting_config.py
+++ sh/pre_config/files/accounting_config.py
@@ -1,5 +1,5 @@
 from models.config_file import ConfigFile
-from helpers.generic_helpers import get_dns_info
+from helpers.generic_helpers import *
 
 
 class AccountingConfig(ConfigFile):
@@ -8,18 +8,36 @@ class AccountingConfig(ConfigFile):
 
     def add_static_parameters(self):
         super().add_static_parameters()
-        self.static_category.add("# Priority halflife is 24h")
+        # Priority halflife is 24h
         self.static_category.add_key_value("PRIORTIY_HALFLIFE", "86400")
 
-        self.static_category.add("# Allow groups to use more than fs")
+        # Allow groups to use more than fs
         self.static_category.add_key_value("GROUP_AUTOREGROUP", "True")
+
         self.static_category.add_key_value("GROUP_ACCEPT_SURPLUS", "True")
-
-        self.static_category.add("# Calculate the surplus allocated to each group correctly")
+        # Calculate the surplus allocated to each group correctly"
         self.static_category.add_key_value("NEGOTIATOR_USE_WEIGHTED_DEMAND", "True")
-
+        self.static_category.add_key_value("NEGOTIATOR_ALLOW_QUOTA_OVERSUBSCRIPTION", "False")
 
     def add_advanced_parameters(self):
         super().add_advanced_parameters()
-        execution_id = self.lightweight_component['execution_id']
+        group_names_quotas = {}
+        voms_config = get_voms_config(self.augmented_site_level_config, self.lightweight_component)
+
+        # VO Based Accounting, FQAN based sub accounting
+        supported_vos = get_supported_vos_with_quotas(self.augmented_site_level_config)
+
+        for vo in supported_vos:
+            name = vo['name']
+            quota = vo['quota']
+            group_names_quotas[f"group_{name}"] = quota
+            fqans = get_fqan_for_vo(name, self.augmented_site_level_config,self.lightweight_component)
+            fqans = get_quotas_for_fqans(fqans)
+            for fqan in fqans:
+                acct_group = generate_acct_group_for_fqan(name, fqan["voms_fqan"])
+                group_names_quotas[acct_group] = fqan["quota"]
+
+        self.advanced_category.add_key_value("GROUP_NAMES", ", \\\n\t".join(group_names_quotas.keys()))
+        for acct_group, quota in group_names_quotas.items():
+            self.advanced_category.add_key_value(f"GROUP_QUOTA_DYNAMIC_{acct_group}", quota)
 
--- sh/pre_config/helpers/generic_helpers.py
+++ sh/pre_config/helpers/generic_helpers.py
@@ -1,6 +1,5 @@
 import yaql
 
-
 engine = yaql.YaqlFactory().create()
 
 
@@ -10,46 +9,181 @@ def get_lightweight_component(data, id):
             return component
 
 
+def get_dns_info(data, id):
+    dns_section = data['dns']
+    dns = None
+    for dns_info in dns_section:
+        if dns_info['execution_id'] == id:
+            dns = dns_info
+            break
+    return dns
+
+
+def get_supported_vos_with_quotas(augmented_site_level_config):
+    supported_vos = augmented_site_level_config['supported_virtual_organizations']
+    zero_quota_vo_count = 0
+    total_defined_quota = 0
+    total_quota = 100
+    for vo in supported_vos:
+        if "quota" not in vo:
+            vo['quota'] = 0
+            zero_quota_vo_count = zero_quota_vo_count+1
+            continue
+        elif "quota" in vo and vo['quota'] == 0:
+            zero_quota_vo_count = zero_quota_vo_count + 1
+            continue
+        else:
+            total_defined_quota += vo['quota']
+    total_undefined_quota = total_quota - total_defined_quota
+    if total_undefined_quota <= 0:
+        return supported_vos
+    per_undefined_vo_quota = total_undefined_quota/zero_quota_vo_count
+    for vo in supported_vos:
+        if vo['quota'] == 0:
+            vo['quota'] = per_undefined_vo_quota
+    return supported_vos
+
+
+def get_quotas_for_fqans(fqans):
+    zero_quota_fqan_count = 0
+    total_defined_quota = 0
+    total_quota = 100
+    for fqan in fqans:
+        if "quota" not in fqan:
+            fqan['quota'] = 0
+            zero_quota_fqan_count = zero_quota_fqan_count + 1
+            continue
+        elif "quota" in fqan and fqan["quota"] == 0:
+            zero_quota_fqan_count = zero_quota_fqan_count + 1
+            continue
+        else:
+            total_defined_quota += fqan["quota"]
+    total_undefined_quota = total_quota - total_defined_quota
+    if total_undefined_quota <= 0:
+        return fqans
+    per_undefined_fqan_quota = total_undefined_quota/zero_quota_fqan_count
+    for fqan in fqans:
+        if fqan["quota"] == 0:
+            fqan["quota"] = per_undefined_fqan_quota
+    return fqans
+
+
+def get_supported_vos(augmented_site_level_config):
+    return [vo['name'] for vo in augmented_site_level_config['supported_virtual_organizations']]
+
+
+def get_fqan_for_vo(vo, augmented_site_level_config, lightweight_component):
+    voms_config = get_voms_config(augmented_site_level_config, lightweight_component)
+    return [fqan for fqan in voms_config if fqan['vo']['name'] == vo]
+
+
+def generate_acct_group_for_fqan(vo, fqan):
+    start_str = f"group_{vo}"
+    if fqan.startswith('/'):
+        fqan = fqan[1:]
+    fqan = fqan.lower().split("role=")
+    group_info = fqan[0]
+    if group_info.endswith('/'):
+        group_info = group_info[:-1]
+    group_info = group_info.replace("/", ".")
+    if len(fqan) == 1:
+        return f"{start_str}.{group_info}"
+
+    role_and_cap = fqan[1]
+    role_and_cap = role_and_cap.split("capability=")
+    role = role_and_cap[0]
+    if len(role_and_cap) == 1:
+        return f"{start_str}.{group_info}_{role}"
+
+    cap = role_and_cap[1].replace(",", "_")
+    return f"{start_str}.{group_info}_{role}_{cap}"
+
+
+def get_primary_user_for_fqan(fqan, augmented_site_level_config, lightweight_component):
+    voms_config = [x for x in get_voms_config(augmented_site_level_config, lightweight_component)
+                   if x['voms_fqan'] == fqan
+                   ]
+    pool_accounts = list(
+        {x['pool_accounts'][y]['base_name']: x['pool_accounts'][y] for x in voms_config
+         for y in range(0, len(x['pool_accounts']))}.values())
+    if len(pool_accounts) == 1:
+        return pool_accounts[0]['base_name']
+    else:
+        for pool_account in pool_accounts:
+            if "sgm" in pool_account['base_name']:
+                return pool_account['base_name']
+        return pool_accounts[len(pool_accounts) - 1]['base_name']
+
+
+def get_users_for_fqan(fqan, augmented_site_level_config, lightweight_component):
+    voms_config = [x for x in get_voms_config(augmented_site_level_config, lightweight_component)
+                   if x['voms_fqan'] == fqan
+                   ]
+    pool_accounts = list(
+        {x['pool_accounts'][y]['base_name']: x['pool_accounts'][y] for x in voms_config
+         for y in range(0, len(x['pool_accounts']))}.values())
+    users = []
+    for pool_account in pool_accounts:
+        users.extend(
+            [pool_account['base_name'] + "{0:0=3d}".format(x) for x in range(1, pool_account['users_num'] + 1)])
+    return users
+
+
+def get_all_linux_users_as_list(augmented_site_level_config, lightweight_component):
+    voms_config = get_voms_config(augmented_site_level_config, lightweight_component)
+    pool_accounts = list(
+        {x['pool_accounts'][y]['base_name']: x['pool_accounts'][y] for x in voms_config
+         for y in range(0, len(x['pool_accounts']))}.values())
+    users = []
+    for pool_account in pool_accounts:
+        users.extend(
+            [pool_account['base_name'] + "{0:0=3d}".format(x) for x in range(1, pool_account['users_num'] + 1)])
+    return users
+
+
+def get_all_fqans(augmented_site_level_config, lightweight_component):
+    voms_config = get_voms_config(augmented_site_level_config, lightweight_component)
+    return list({x['voms_fqan']: x for x in voms_config}.values())
+
+
+def analyze_fqan(fqan):
+    if len(fqan) == 0:
+        return ""
+
+
 def evaluate(data, query):
     expression = engine(query)
     return expression.evaluate(data)
 
 
-def get_voms_config(data, component_section):
+def get_voms_config(augmented_site_level_config, lightweight_component):
     default_voms_config = []
     try:
-        supported_vos = component_section['config']['vos']
+        supported_vos = lightweight_component['config']['vos']
     except KeyError:
-        supported_vos = evaluate(data, "$.supported_virtual_organizations")
+        supported_vos = evaluate(augmented_site_level_config, "$.supported_virtual_organizations")
     try:
-        voms_config = evaluate(data, "$.voms_config")
+        voms_config = evaluate(augmented_site_level_config, "$.voms_config")
     except KeyError:
         for vo in supported_vos:
             default_pool_accounts = []
             default_pool_account_key = "default_pool_account_" + vo['name']
             default_pool_account__sgm_key = "default_pool_account_" + vo['name'] + "sgm"
-            if default_pool_account_key in data:
-                default_pool_accounts.append(data[default_pool_account_key])
+            if default_pool_account_key in augmented_site_level_config:
+                default_pool_accounts.append(augmented_site_level_config[default_pool_account_key])
                 default_voms_config.append({
                     'voms_fqan': "/{vo_name}".format(vo_name=vo['name']),
                     'vo': vo,
                     'pool_accounts': default_pool_accounts
                 })
-            if default_pool_account__sgm_key in data:
-                default_pool_accounts.append(data[default_pool_account__sgm_key])
+            if default_pool_account__sgm_key in augmented_site_level_config:
+                default_pool_accounts.append(augmented_site_level_config[default_pool_account__sgm_key])
                 default_voms_config.append({
-                    'voms_fqan': '/{vo_name}/ROLE=lcgadmin',
+                    'voms_fqan': '/{vo_name}/Role=lcgadmin',
                     'vo': vo,
                     'pool_accounts': default_pool_accounts
                 })
         voms_config = default_voms_config
+    for config in voms_config:
+        config['voms_fqan'] = config['voms_fqan'].replace("ROLE", 'Role')
     return voms_config
-
-
-def get_dns_info(data, id):
-    dns = None
-    for dns_info in data['dns']:
-        if int(dns_info['execution_id']) == id:
-            dns = dns_info
-            break
-    return dns
